51d0cf16e7e2249204d2d06024dfe8b89030144a
jiangyouli <jiangyouli@senyint.rh>
2016-08-24 14:33:10 +0800

    create Test

---
+++ gitTest/gitTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitTest
+//
+//  Created by 蒋友利 on 16/8/24.
+//  Copyright © 2016年 蒋友利. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
